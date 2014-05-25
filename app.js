be4f6c1c8a135ca908fb2c7f279e4bb5d11987d5
Joseph <joe@josephmccarron.com>
2014-05-25 05:51:44 -0700

    prepping for phase 2 - the nav bar app

--- app.js
+++ app.js
@@ -21,7 +21,13 @@
         this.ajax('getComments');
       },
       'click #modal_toggle':'showModal',
-      'click .done':'init'
+      'click .done':'init',
+      // nav bar events
+      'pane.activated':'onPaneActivated',
+
+      'click li.tab':'onTabClicked',
+      'click .imploded':'toggleRow',
+      'click button.get_questions':'getHCquestions'
     },
     requests: {
       getUser: function() {
@@ -57,11 +63,33 @@
           proxy_v2: true,
           data: article
         };
+      },
+
+      // nav bar requests
+      getHCquestions: function (answered) {
+        var accepted = '';
+        if (answered == 'yes') {
+          accepted = '&accepted=true';
+        } else if (answered == 'no') {
+          accepted = '&accepted=false';
+        }
+        return {
+          url: '/api/v2/help_center/questions.json?include=users,communities' + accepted, // add topics to sideload when implemented
+          type: 'GET',
+          success: function(response){this.gotHCQuestions(response, answered);}
+        };
       }
     },
     init: function(e) {
       if (e) { e.preventDefault(); }
       this.switchTo('default', {});
+    },
+    // load the navbar app if the pane is activated for the first time
+    onPaneActivated: function(data) {
+      if(data.firstLoad) {
+        this.loadNavBar();
+
+      }
     },
     fetchComments: function(data) {
       var currentUser = data.user;
@@ -173,17 +201,19 @@
       if (e) { e.preventDefault(); }
 
 
-
+      // gather field valudes
       var label_names = this.$('input.labels').val().split(/\W/),
           draft = this.$('input.draft').prop("checked"),
           promoted = this.$('input.promoted').prop("checked"),
           comments_disabled = this.$('input.comments_disabled').prop("checked"),
           locale = this.$('select.locale').val(),
+          // since it is only placeholder text, and not a value, I had to add the following two lines to properly post the default title
           ticket_id = this.ticket().id(),
           default_title = helpers.fmt('From ticket #%@ via Ticket to Help Center App', ticket_id),
           title = (this.title || default_title),
-          html_single_quotes = this.html.replace(/"/gm, "'"),
+          html_single_quotes = this.html.replace(/"/gm, "'"), //replace double quotes with single quotes
           body = html_single_quotes.replace(/(\r\n|\n|\r)/gm," "), //remove line breaks
+          //TODO: add the option to post a comment to an article, or an answer to a question
           article_data = {"article": {
             "label_names": label_names,
             "draft": draft,
@@ -193,15 +223,17 @@
           }},
           article = JSON.stringify(article_data),
           section = this.$('select.section').val();
-
+      // field validation
       if(!section) {
         services.notify("No section specified. Please choose a section before submitting.", "error");
         return;
       }
+      // field validation
       if(!locale) {
         services.notify("No locale specified. Please choose a locale before submitting.", "error");
         return;
       }
+      // post the article
       this.ajax('postArticle', article, section)
       .done(function(response){
         var postedArticle = response.article;
@@ -230,5 +262,108 @@
     postArticleFail: function(data) {
       services.notify('Failed to post to Help Center. Please check that you have permission to create an article in the chosen section and try reloading the app.', 'error');
     },
+
+    // #NAVBAR
+
+    loadNavBar: function() {
+      if (this.setting("restrict_to_moderators") === true) {
+        var me = this.currentUser();
+        if (me.moderator !== true) {
+          services.notify('This app is restrcited to Moderators, and you are not one. Please check with an Admin about getting Moderator privileges.');
+          return;
+          // this.switchTo('nav_restricted')
+        }
+      }
+      //TODO load the user's last tab choice from local storage, then load and select that
+
+      this.switchTo('nav_home', {
+      });
+      this.$('div.app_frame').parents('.apps_nav_bar').css("padding", "0");
+      // currently defaulting to questions via the following code
+      
+      this.getHCquestions();
+    },
+    onTabClicked: function(e) {
+      if(e) {e.preventDefault();}
+      var tab = e.currentTarget.id;
+      console.log("Tab: " + tab);
+
+      // select the clicked tab
+      var selector = '#' + tab;
+      this.$('li.tab').removeClass('active');
+      this.$(selector).addClass('active');
+      this.$('.tab_content').html('<div class="spinner dotted"></div>');
+
+      // TODO: get the corresponding objects, the success of which should render and inject the content
+      if (tab == 'questions') {
+        this.ajax('getHC' + tab);
+      } else {
+        // render an error whenever an unsupported tab is chosen
+        this.$('.tab_content').html('<br><div class="alert alert-error"><b>Not yet supported</b><br> Only Questions are currently supported, sorry... &nbsp;&nbsp;As the API for Help Center develops so will this app.</div>');
+      }
+      // 
+    },
+    getHCquestions: function(e) {
+      var answered = 'no';
+      if(e) { // if triggered by a button click...
+        e.preventDefault();
+        // gather the filter form inputs
+        var answeredYes = this.$('input.answered_yes').prop("checked"),
+          answeredNo = this.$('input.answered_no').prop("checked");
+        if (answeredYes && answeredNo) {
+          answered = '';
+        } else if (answeredYes && !answeredNo) {
+          answered = 'yes';
+        } else if (!answeredYes && answeredNo) {
+          answered = 'no';
+        }
+      }
+      this.$('.questions_table').html('<div class="spinner dotted"></div>');
+      this.ajax('getHCquestions', answered);
+    },
+    gotHCQuestions: function(response, answered) {
+      console.log('Got questions!');
+      console.log('Answered? ' + answered);
+      var questions = response.questions,
+        communities = response.communities;
+      // TODO: process returned data e.g. author_id to author, accepted_answer_id to answer, community_id to community, topic_ids to topics
+      var singleCommunity = false;
+      if (communities.length == 1) {
+        singleCommunity = true;
+      }
+      var answeredYes,
+        answeredNo;
+      if (answered == 'yes') {
+        answeredYes = true;
+        answeredNo = false;
+      } else if (answered == 'no') {
+        answeredYes = false;
+        answeredNo = true;
+      } else {
+        answeredYes = true;
+        answeredNo = true;
+      }
+      
+      // render and inject the relevant tab content
+      var html = this.renderTemplate('nav_HC_questions', {
+        questions: questions,
+        communities: communities,
+        singleCommunity: singleCommunity,
+        answeredYes: answeredYes,
+        answeredNo: answeredNo
+      });
+      this.$('.tab_content').html(html);
+      this.$('#questions').addClass('active');
+      this.$("[rel=tooltip]").tooltip({ placement: 'right'});
+    },
+    toggleRow: function(e) {
+
+      var id = e.currentTarget.id;
+      var implodedSelector = '#' + id;
+      this.$(implodedSelector).toggleClass("selected_to_explode");
+      var explodedSelector = '.' + id;
+      this.$(explodedSelector).toggleClass("hidden");
+    }
+
   };
 }());
